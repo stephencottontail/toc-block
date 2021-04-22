574a73843217a896c782c1aa51e1ea01ed3eb72a
Stephen Dickinson <stephencottontail@me.com>
2021-04-22 11:13:34 -0600

    Quick changes to `eslint` config
    Add linting for hooks and suppress error about React not being in scope

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,27 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+	env: {
+		node: true,
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:react-hooks/recommended",
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: "module",
+	},
+	plugins: ["react"],
+	rules: {
+		/**
+		 * WP does some black magic to make `React` available
+		 * without having to explicitly declare it
+		 */
+		"react/react-in-jsx-scope": [0],
+	},
 };
